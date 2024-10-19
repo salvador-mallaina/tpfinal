7da343eb3a14e1f0aaf1cbfebbe809053c8e4800
salvador-mallaina <168142127+salvador-mallaina@users.noreply.github.com>
2024-10-19 00:33:26 -0300

    Add files via upload

--- tpfinalparte1/funciones.js
+++ tpfinalparte1/funciones.js
@@ -6,21 +6,28 @@ function reload() {
 
   textSize(tamTexto);
 }
-function pantalla1Boton(imagen) {
+function pantalla1Boton(imagen,texto) {
   fill(200, 50, 40);
   image(misImagenes[imagen], 0, 0, anchoPantalla, altoPantalla);
   rect( posXBoton, posYBoton, anchoBoton, altoBoton );
   fill(0, 255, 255);
-  
+  text( misTextos[texto],  inicioTexto,   inicioTexto,   anchoTexto   );
 }
-function pantallaCon2Botones(imagen) {
+function pantallaCon2Botones(imagen,texto) {
   fill(255);
   image(misImagenes[imagen], 0, 0, anchoPantalla, altoPantalla);
   rect(160, 340, anchoBoton, altoBoton);
   rect(480, 340, anchoBoton, altoBoton);
+    text( misTextos[texto],  inicioTexto,   inicioTexto,   anchoTexto   );
+     fill(0);
+  text(accion2,170,395);
+   text(accion1,495,395);
 }
-function pantallaFinal(imagen) {
+function pantallaFinal(imagen,texto) {
   fill(0, 150, 0),
     image(misImagenes[imagen], 0, 0, anchoPantalla, altoPantalla);
   rect(270, 340, 100, 50);
+   text( misTextos[texto],  inicioTexto,   inicioTexto,   anchoTexto   );
+    fill(0);
+  text(accion3,290,380);
 }
--- tpfinalparte1/tpfinalparte1.js
+++ tpfinalparte1/tpfinalparte1.js
@@ -4,7 +4,10 @@ let misTextos =[];
 let  anchoTexto ;
 let tamTexto;
 let  inicioTexto ;
-let posTextoActual=0
+let accion1 ="Si"
+let accion2="No"
+let accion3="Fin"
+
 
   let pantallaActual =0;
 let posXBoton =580;
@@ -22,14 +25,14 @@ function preload() {
   for (let i=0; i<cantImgs; i++) {
     misImagenes[i]=loadImage("data/Designer"+i+".jpg");
   }
-  misTextos[posTextoActual]=  loadStrings('data/aventura.txt');
-}
+  misTextos =  loadStrings('data/aventura.txt');
 
+soundFormats('mp3');
 musica=loadSound("data/mario.mp3")
-
+}
 function setup() {
   createCanvas( anchoPantalla, altoPantalla);
-  musica.loop();
+ 
 }
 
 
@@ -37,46 +40,52 @@ function draw() {
   background(0);
   reload();
   if (pantallaActual==0) {
-    pantalla1Boton(0)
+    pantalla1Boton(0,0 )
   } else if (pantallaActual==1) {
-    pantalla1Boton(1)
+    pantalla1Boton(1,1)
   } else if (pantallaActual==2) {
-    pantalla1Boton(2)
+    pantalla1Boton(2,2)
   } else if (pantallaActual==3) {
-    pantallaCon2Botones(3)
+    pantallaCon2Botones(3,3)
   } else if (pantallaActual==4) {
-    pantalla1Boton(4)
+    pantalla1Boton(4,4)
   } else if (pantallaActual==5) {
-    pantalla1Boton(5)
+    pantalla1Boton(5,5)
   } else if (pantallaActual==6) {
-    pantallaFinal(6)
+    pantallaFinal(6,6)
   } else if (pantallaActual==7) {
-    pantalla1Boton(7)
+    pantalla1Boton(7,7)
   } else if (pantallaActual==8) {
-    pantalla1Boton(8)
+    pantalla1Boton(8,8)
   } else if (pantallaActual==9) {
-    pantallaCon2Botones(9)
+    pantallaCon2Botones(9,9)
   } else if (pantallaActual==10) {
-    pantalla1Boton(10)
+    pantalla1Boton(10,10)
   } else if (pantallaActual==11) {
-    pantalla1Boton(11)
+    pantalla1Boton(11,11)
   } else if (pantallaActual==12) {
-    pantallaFinal(12)
+    pantallaFinal(12,12)
   } else if (pantallaActual==13) {
-    pantalla1Boton(13)
+    pantalla1Boton(13,13)
   } else if (pantallaActual==14) {
     y = 1
       t= -4
-      pantallaCon2Botones(14)
+      pantallaCon2Botones(14,14)
   } else if (pantallaActual==15) {
-    pantalla1Boton(15)
+    pantalla1Boton(15,15)
   } else if (pantallaActual==16) {
-    pantalla1Boton(16)
+    pantalla1Boton(16,16)
   } else if (pantallaActual==17) {
-    pantallaFinal(17)
+    pantallaFinal(17,17)
   }
 }
 function mousePressed() {
+  if(musica.isPlaying()){
+    musica.pause();
+  }else{
+    musica.play();
+  }
+    
   clickBotonCuadrado();
   clickBotonNo();
     clickBotonSi();
